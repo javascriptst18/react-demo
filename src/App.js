f1460ccdcdabbcea4ea526d0282c40e212f6e181
Jesper Orb <jesper.orb@zocom.se>
2018-08-06 13:33:41 +0200

    Props and object spread

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component, Fragment } from 'react';
-import Header from './components/Header';
 import Paragraph from './components/Paragraph';
-let age = 10;
+
+let person = {
+  name:"Jesper",
+  hobby: "coding"
+}
 
 function App(){
   return (
-    <div className="container">
-      <Header 
-        name="Jesper" 
-        hobby="coding"
-        age={age} />
-      <Header name="Bananis" />
+    <div>
+      <Header {...person} show/>
+      <Header name="Jesper" hobby="coding" age={10} />
       <Paragraph />
-    </div >
+    </div>
   );
+}
+
+function Header(props) {
+  if(props.name === "Jesper"){
+    return <h1 className="header">
+      {props.name} - {props.hobby}
+    </h1>;
+  } else {
+    return null;
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-function Header(props) {
-  return <h1 className="header"> {props.name} - {props.hobby} </h1>;
-}
+
 
 export default Header;
