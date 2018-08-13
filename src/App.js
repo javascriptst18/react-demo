4c9dd88d098b68a8dfab394e7e716c0bb4e9af60
Jesper Orb <jesper.orb@zocom.se>
2018-08-13 10:23:53 +0200

    Card Demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import FetchData from './components/FetchData';
-import Counter from './components/Counter';
 
-// Class is used when state/lifecycle is used
 class App extends React.Component {
-
-  // Class property
-  state = {
-    counter: 0
-  }
-
-  handleClick = (event) => {
-    // Use ONLY this.setState()
-    this.setState({ counter: this.state.counter + 1 });
-  }
-
   render(){
-    return <Counter />;
+    // Card has no className or onClick
+    return <CrimsonCard 
+      title="My Title" 
+      subtitle="My subtitle"
+      />;
   }
 }
+
+// Component
+function Card(props){
+  const composedClassNames = `card ${props.customClass}`;
+  // Div has a class
+  // Style takes an object as argument
+  return <div className={composedClassNames} style={ { backgroundColor: props.color } }>
+      <img src={props.img} />
+      <h1>{ props.title }</h1>
+      <em>{ props.subtitle }</em>
+    </div>
+}
+
+// Higher Order Component
+function CrimsonCard(props){
+  // return <Card {...props} color="crimson" />
+  return <Card title={props.title} subtitle={props.subtitle} color="crimson" />
+}
+
+
+
+
+
 
 
 export default App;
